3cc57b3a1602ad8f102fb3fd3bdf7397c0e34f28
ramazan.demir9 <ramazan.demir9@icisleri.gov.tr>
2021-12-23 11:26:44 +0300

    customer spring rest api and test

---
+++ src/test/java/guru/springframework/msscbrewery/web/controller/BeerControllerTest.java
@@ -0,0 +1,21 @@
+package guru.springframework.msscbrewery.web.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+class BeerControllerTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBeer() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void handlePost() {
+    }
+}
